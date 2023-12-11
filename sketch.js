b671f32f7fd5a64844fcf58aeacb83af960b44e0
OmarAl-Omari <omaralomarijor@gmail.com>
2023-12-11 14:28:33 -0600

    Practice

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
+let a = 10, b=4,c=5;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
